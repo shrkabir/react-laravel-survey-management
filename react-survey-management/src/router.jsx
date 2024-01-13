f4c2584311b80e5b91083d5da742cc8aec771087
Ahmed Shahriar Kabir <sazzad.shrkabir@gmail.com>
2024-01-13 22:03:43 +0600

    signup, signin, dashboard, survey components, defaultlayout, guest layout

--- react-survey-management/src/router.jsx
+++ react-survey-management/src/router.jsx
@@ -1,10 +1,43 @@
-import {createBrowserRouter} from "react-router-dom";
-import App from './App';
+import {Navigate, createBrowserRouter} from "react-router-dom";
+import Dashboard from './views/Dashboard';
+import Surveys from "./views/Surveys";
+import Signup from "./views/Signup";
+import Login from './views/Login';
+import GuestLayout from "./components/GuestLayout";
+import DefaultLayout from "./components/DefaultLayout";
 
 const router= createBrowserRouter([
     {
-        path: '/login',
-        element: <App />
+        path: '/',
+        element: <DefaultLayout/>,
+        children: [
+            {
+                path: '/dashboard',
+                element: <Navigate to="/"/>
+            },
+            {
+                path: '/',
+                element: <Dashboard />
+            },
+            {
+                path: '/surveys',
+                element: <Surveys/>
+            },
+        ]
+    },
+    {
+        path: '/',
+        element: <GuestLayout />,
+        children: [
+            {
+                path: 'login',
+                element: <Login/>
+            },
+            {
+                path: 'signup',
+                element: <Signup/>
+            }
+        ]
     }
 ]);
 
