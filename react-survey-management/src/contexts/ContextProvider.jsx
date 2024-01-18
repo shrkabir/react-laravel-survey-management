5b0fbe7c9922585f737979de787bad47524ed8af
Ahmed Shahriar Kabir <sazzad.shrkabir@gmail.com>
2024-01-18 21:50:27 +0600

    use userStateContext in Default layout

--- react-survey-management/src/contexts/ContextProvider.jsx
+++ react-survey-management/src/contexts/ContextProvider.jsx
@@ -1,11 +1,25 @@
-import {useState, useContext} from 'react';
+import {useState, useContext, createContext} from 'react';
 
-const StateContext = useContext({children});
-export const ContextProvider = ()=>{
-    const [currentUser, setcurrentUser] = useState({});
+const StateContext = createContext({
+    currentUser : {},
+    userToken : null,
+    setcurrentUser : ()=> {},
+    setUserToken : ()=> {}
+
+}); //declear context
+
+export const ContextProvider = ({children})=>{
+    const [currentUser, setcurrentUser] = useState({
+        name: 'Tom Cook',
+        email: 'tom@example.com',
+        imageUrl:
+        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    }); //declear state to pass into context provider
+
     const [userToken, setUserToken] = useState(null);
+    
     return (
-        <StateContext.Provider value={{
+        <StateContext.Provider value={{  //context provider
             currentUser,
             setcurrentUser,
             userToken,
@@ -15,3 +29,5 @@ export const ContextProvider = ()=>{
         </StateContext.Provider>
     )
 }
+
+export const userStateContext = ()=> useContext(StateContext);
