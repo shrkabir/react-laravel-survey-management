8fd79b695a30f55bb884beaafc1001b3f0bd7d92
Ahmed Shahriar Kabir <sazzad.shrkabir@gmail.com>
2024-01-19 00:09:32 +0600

    authenticate using usertoken

--- react-survey-management/src/components/DefaultLayout.jsx
+++ react-survey-management/src/components/DefaultLayout.jsx
@@ -1,7 +1,7 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, UserIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import { NavLink, Outlet } from 'react-router-dom'
+import { NavLink, Navigate, Outlet } from 'react-router-dom'
 import { userStateContext } from '../contexts/ContextProvider'
 
 const navigation = [
@@ -14,7 +14,11 @@ function classNames(...classes) {
 }
 
 export default function DefaultLayout() {
-    const {currentUser} = userStateContext();
+    const {currentUser, userToken} = userStateContext();
+    
+    if(!userToken){
+        return <Navigate to='login'></Navigate>
+    }
     const logout = (ev) => {
         ev.preventDefault();
         console.log("logout");
--- react-survey-management/src/components/GuestLayout.jsx
+++ react-survey-management/src/components/GuestLayout.jsx
@@ -1,6 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { userStateContext } from "../contexts/ContextProvider";
 
 export default function () {
+    const {userToken} = userStateContext();
+    if(userToken){
+        return <Navigate to='/'></Navigate>
+    }
     return (
         <div>
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
